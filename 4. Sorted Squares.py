f9262eea9c0196ddcce3d8f0413e6c85dfabcd2a
Deepa Pandey <deepapandey364@gmail.com>
2022-01-12 00:49:48 +0530

    done

--- 4. Sorted Squares.py
+++ 4. Sorted Squares.py
@@ -2,7 +2,9 @@ Given an integer array nums sorted in non-decreasing order, return an array of t
 do it in O(n).
  
   
-# Approach 1:
+# Approach 1: 
+# Using Python Bisect to find the position to keep the element, so that the array remains sorted.
+
 class Solution(object):
     def sortedSquares(self, nums):
         """
@@ -29,3 +31,40 @@ class Solution(object):
         
         res = [i*i for i in positiveList]
         return res
+       
+   =========================================================================================================
+   # Approach 2:
+   # Using two pointers technique
+   # Took less time than the first approach
+   
+   class Solution(object):
+    def sortedSquares(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        left = 0
+        right = len(nums) - 1
+        answer = [None]*len(nums)
+        for i in range(len(nums)-1,-1,-1):
+            if abs(nums[left]) > abs(nums[right]):
+                answer[i] = nums[left]**2
+                left += 1
+                
+            else:
+                answer[i] = nums[right]**2
+                right-=1
+        return answer
+                
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
