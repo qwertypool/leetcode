d5fc767a6a5f3f123c7239263496e089c12df34d
Deepa Pandey <deepapandey364@gmail.com>
2022-01-15 19:38:34 +0530

    done

--- Data Structures/Linked List/4. Reverse LL.py
+++ Data Structures/Linked List/4. Reverse LL.py
@@ -26,3 +26,30 @@ class Solution(object):
         
         head = prev
         return head
+
+      
+ -----------------------------------------------------------------------------------------------
+# Approach 2:
+# Recursive approach:
+# The idea is to change next with prev, prev with current, and current with next.  
+      
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def reverseList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        return self.recursiveReverseList(head)
+
+        
+    def recursiveReverseList(self, head, prev = None):
+        if not head:
+            return prev
+        temp = head.next
+        head.next = prev
+        return self.recursiveReverseList(temp, head)
