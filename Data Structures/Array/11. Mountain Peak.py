f507b698e2ed7bcc7545ebea55e1c44e43640159
Deepa Pandey <deepapandey364@gmail.com>
2022-01-25 17:17:26 +0530

    done

--- Data Structures/Array/11. Mountain Peak.py
+++ Data Structures/Array/11. Mountain Peak.py
@@ -9,8 +9,11 @@ There exists some i with 0 < i < arr.length - 1 such that:
 arr[0] < arr[1] < ... < arr[i - 1] < arr[i]
 arr[i] > arr[i + 1] > ... > arr[arr.length - 1]
 
-
-
+========================================================
+Two people climb from left and from right separately.
+If they are climbing the same mountain,
+they will meet at the same point.
+=======================================================
 class Solution(object):
     def validMountainArray(self, arr): 
         mountEverest = -1
@@ -36,3 +39,17 @@ class Solution(object):
                 return False
         return True
                 
+===============================================================
+# Approach 2:
+# i=0 and j= len(num)-1
+# Go up the mountain from i 
+# Go up the mountain from j
+# meet at a point i=j and, i is not 0 and, j is not length of num
+        
+    def validMountainArray(self, A):
+        i, j, n = 0, len(A) - 1, len(A)
+        while i + 1 < n and A[i] < A[i + 1]: i += 1
+        while j > 0 and A[j - 1] > A[j]: j -= 1
+        return 0 < i == j < n - 1
+        
+  
